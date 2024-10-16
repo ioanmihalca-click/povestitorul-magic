b01aae2b799df7440df5748714ccd07289a50c5b
Ioan "Click" Mihalca <164161041+ioanmihalca-click@users.noreply.github.com>
2024-10-16 17:33:13 +0300

    evo

--- resources/views/livewire/story-generator.blade.php
+++ resources/views/livewire/story-generator.blade.php
@@ -1,5 +1,5 @@
 <div class="py-8">
-    <div class="max-w-4xl ">
+    <div class="max-w-4xl mx-auto">
         <div class="overflow-hidden bg-white shadow-lg rounded-3xl">
             <div class="p-4 bg-gradient-to-br from-blue-100 via-purple-100 to-pink-100">
                 <h1 class="mb-6 text-3xl font-bold text-center text-indigo-600">Atelierul Povestitorului Magic</h1>
