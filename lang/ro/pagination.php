2fe6b802292137220c62a504b8878441896b8c24
Ioan "Click" Mihalca <164161041+ioanmihalca-click@users.noreply.github.com>
2024-10-21 12:40:49 +0300

    prompts

---
+++ lang/ro/pagination.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'previous' => '&laquo; Înapoi',
+    'next' => 'Înainte &raquo;',
+    'Showing' => 'Se afișează',
+    'to' => 'până la',
+    'of' => 'din',
+    'results' => 'rezultate',
+];
